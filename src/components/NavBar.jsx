39e127ae4874d498ecd0ce5770e1fd37adcab895
Anna-J-o-s-e <annajose941@gmail.com>
2024-06-14 04:34:23 +0530

    buspage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
 
-        
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="#">KSRTC</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link active" aria-current="page" to="#">Add Bus</Link>
+        <Link className="nav-link" to="#">Search Bus</Link>
+        <Link className="nav-link" to="#">Delete Bus</Link>
+        <Link className="nav-link" to="">View Buses</Link>
+      </div>
+    </div>
+  </div>
+</nav>
     </div>
   )
 }
